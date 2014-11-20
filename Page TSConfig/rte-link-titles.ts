22460d4dd200b57847a7e09000b47ff182d78c90
Matthias Weber <mweber@q4u.de>
2014-11-20 06:20:34 +0100

    link titles

---
+++ Page TSConfig/rte-link-titles.ts
@@ -0,0 +1,10 @@
+# remove default link titles for RTE links
+RTE {
+	classesAnchor {
+		internalLink.titleText =
+		externalLink.titleText =
+		externalLinkInNewWindow.titleText =
+		internalLinkInNewWindow.titleText =
+		download.titleText =
+	}
+}
