7f16502109b65d16a7b68751af817e4059782426
Matthias Weber <mweber@q4u.de>
2014-11-20 06:31:15 +0100

    imagewidths

---
+++ TypoScript templates/setup-imagewidths.ts
@@ -0,0 +1,29 @@
+tt_content.image.20 {
+	maxW >
+	maxWInText >
+	maxW.cObject = CASE
+	maxW.cObject {
+		key.data = levelfield:-1, backend_layout_next_level, slide
+		key.override.field = backend_layout
+		# backend layout with id=1
+		1 = CASE
+		1 {
+			key.field = colPos
+			# colPos=0
+			0 = TEXT
+			0.value = 600
+			1 = TEXT
+			1.value = 200
+		}
+		# copy the values from backend layout 1
+		2 < .1
+		2 {
+			# adjust where necessary
+			1.value = 400
+			2.value = 400
+		}
+	}
+
+	# copy the same configuration to maxWInText; can be overridden afterwards
+	maxWInText < .maxW
+}
