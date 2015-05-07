c6db6cc42d960760f450cae232b5aca97cf519b4
Matthias Weber <mweber@q4u.de>
2015-05-07 08:28:42 +0200

    open graph for default pages and tt_news

--- opengraph-constants.ts
+++ opengraph-constants.ts
@@ -1,16 +1,12 @@
-# http://www.it-weblog.de/2012/02/typo3-snippet-open-graph/
-
-canonicalbase = http://{$domain}/
-[treeLevel = 0]
-canonicalbase = http://{$domain}
-[global]
-
 timeformat = %d.%m.%Y %T
 
-og.logo = fileadmin/tmpl/img/logo-og.png
-og.titlesuffix = Title suffix
-og.description = My default description
-og.locale = de_DE
-og.type = website
-domain = www.url.de
-baseurl = http://{$domain}/
+og {
+	logo = fileadmin/tmpl/img/logo-og.png
+	titlesuffix = Title suffix
+	description = Default description
+	locale = de_DE
+	type = website
+
+	maxW = 1200
+	maxH = 630
+}
--- opengraph-setup.ts
+++ opengraph-setup.ts
@@ -1,48 +1,154 @@
-# http://www.it-weblog.de/2012/02/typo3-snippet-open-graph/
+# open graph meta tags for pages and tt_news
+page.headerData.40 = COA
+page.headerData.40 {
 
-# Temp Stuff
-temp.canonical = COA
-temp.canonical.wrap = <link rel="canonical" href="|">
-temp.canonical.1 = TEXT
-temp.canonical.1 {
-	value = {$canonicalbase}
-	wrap = |
-}
-temp.canonical.2 = TEXT
-temp.canonical.2 {
-	typolink {
-		parameter.data = TSFE:id
-		returnLast = url
-		addQueryString = 0
-		addQueryString.method = GET
+	# og:url
+	10 = TEXT
+	10 {
+		typolink.parameter.data = TSFE:id
+		typolink.forceAbsoluteUrl = 1
+		typolink.returnLast = url
+		wrap = <meta property="og:url" content="|">
 	}
+
+	# og:type
+	20 = TEXT
+	20.value = <meta property="og:type" content="{$og.type}">
+
+	# og:title
+	30 = TEXT
+	30.field = subtitle // title
+	30.wrap = <meta property="og:title" content="|&nbsp;&#124;&nbsp;{$og.titlesuffix}">
+
+	# og:locale
+	40 = TEXT
+	40.value = <meta property="og:locale" content="{$og.locale}">
+
+	# og:image
+	50 = COA
+	50 {
+		10 = CONTENT
+		10 {
+			table = tt_content
+			select {
+				where = colPos=0
+				orderBy = sorting
+			}
+			renderObj = COA
+			renderObj {
+				10 = FILES
+				10 {
+					sorting = sorting_foreign
+					references {
+						table = tt_content
+						fieldName = image
+					}
+					renderObj = IMG_RESOURCE
+					renderObj {
+						file {
+							treatIdAsReference = 1
+							import.data = file:current:uid
+							maxW = {$og.maxW}
+							maxH = {$og.maxH}
+						}
+						stdWrap.wrap = <meta property="og:image" content="http://{$hostname}/|">
+					}
+				}
+			}
+		}
+		20 = IMG_RESOURCE
+		20 {
+			file {
+				treatIdAsReference = 1
+				import.data = levelmedia:-1, slide
+				import.listNum = 0
+				maxW = {$og.maxW}
+				maxH = {$og.maxH}
+			}
+			stdWrap.wrap = <meta property="og:image" content="http://{$hostname}/|">
+		}
+	}
+
+	# og:description
+	60 = TEXT
+	60.field = description
+	60.ifEmpty = {$og.description}
+	60.wrap = <meta property="og:description" content="|">
+
+	# og:updated_time
+	70 = TEXT
+	70 {
+		data = register : SYS_LASTCHANGED
+		wrap = <meta property="og:updated_time" content="|">
+	}
+
 }
-# Canonical
-page.headerData.15 < temp.canonical
-# OpenGraph Data
-page.headerData.30 = TEXT
-page.headerData.30.value (
-<meta property="og:image" content="{$baseurl}{$og.logo}">
-<meta property="og:locale" content="{$og.locale}">
-<meta property="og:type" content="{$og.type}">
-)
-page.headerData.31 = TEXT
-page.headerData.31.field = title
-page.headerData.31.wrap = <meta property="og:title" content="|, {$og.titlesuffix}">
-page.headerData.32 = TEXT
-page.headerData.32.field = description
-page.headerData.32.ifEmpty = {$og.description}
-page.headerData.32.wrap = <meta property="og:description" content="|">
-page.headerData.33 < temp.canonical
-page.headerData.33.wrap = <meta property="og:url" content="|">
-page.headerData.34 = TEXT
-page.headerData.34 {
-	data = register : SYS_LASTCHANGED
-	wrap = <meta property="og:updated_time" content="|">
-}
-page.headerData.35 = TEXT
-page.headerData.35 {
-	data = register : SYS_LASTCHANGED
-	strftime = {$timeformat}
-	wrap = <meta property="human_updated_time" content="|">
-}
+
+# tt_news
+[globalVar = GP:tx_ttnews|tt_news > 0]
+
+	page.headerData.40 {
+
+		# og:url
+		10 = TEXT
+		10 {
+			typolink.parameter.data = TSFE:id
+			typolink.forceAbsoluteUrl = 1
+			typolink.returnLast = url
+			typolink.additionalParams.cObject = COA
+			typolink.additionalParams.cObject {
+				10 = TEXT
+				10.dataWrap = &tx_ttnews[tt_news]={GP:tx_ttnews|tt_news}
+				10.if.isTrue.data = GP:tx_ttnews|tt_news
+				20 = TEXT
+				20.dataWrap = &tx_ttnews[cat]={GP:tx_ttnews|cat}
+				20.if.isTrue.data = GP:tx_ttnews|cat
+			}
+			wrap = <meta property="og:url" content="|">
+		}
+
+		# og:title
+		30 = RECORDS
+		30 {
+			source = {GP:tx_ttnews|tt_news}
+			source.insertData = 1
+			tables = tt_news
+			conf.tt_news >
+			conf.tt_news = TEXT
+			conf.tt_news.field = title
+		}
+
+		# og:image
+		50 = RECORDS
+		50 {
+			tables = tt_news
+			source.data = GP:tx_ttnews|tt_news
+			conf.tt_news = IMG_RESOURCE
+			conf.tt_news {
+				file {
+					import = uploads/pics/
+					import.field = image
+					import.listNum = 0
+				}
+				stdWrap.required = 1
+				stdWrap.wrap = <meta property="og:image" content="{TSFE:baseUrl}|">
+				stdWrap.insertData = 1
+			}
+		}
+
+		# og:description
+		60.override.data = register:newsSubheader
+
+		# og:updated_time
+		70.override.cObject = RECORDS
+		70.override.cObject {
+			source = {GP:tx_ttnews|tt_news}
+			source.insertData = 1
+			tables = tt_news
+			conf.tt_news = TEXT
+			conf.tt_news.field = tstamp
+		}
+
+	}
+
+[global]
